079cf0fcc7ed8c90dcb72aea6ce8b6ab22b226ce
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2024-04-22 13:26:41 +0800

    f3

---
+++ timer.py
@@ -0,0 +1,43 @@
+import asyncio
+import logging
+import os
+import sys
+from datetime import datetime, timedelta, timezone
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+from main import main as run_task
+
+logging.basicConfig(
+    level=logging.INFO, stream=sys.stdout, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+async def main():
+    scheduler = AsyncIOScheduler()
+
+    now_ = datetime.now(timezone(timedelta(hours=8)))
+
+    # 设定结束时间，防止任务被遗忘后一直运行下去
+    end_date_ = now_ + timedelta(days=30)
+
+    # Default to 1onc /20min
+    seconds_ = 60 * 20
+    if (interval_seconds := os.getenv("INTERVAL_SECONDS")) and interval_seconds.isdigit():
+        seconds_ = int(interval_seconds)
+
+    trigger = IntervalTrigger(seconds=seconds_, end_date=end_date_)
+
+    scheduler.add_job(run_task, trigger=trigger, kwargs={"headless": True}, max_instances=2, next_run_time=now_)
+    scheduler.start()
+
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
